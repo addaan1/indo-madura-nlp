3f2ed530f871793e9df2a3ab47717e3e026e7770
adi <adieeffendy5@gmail.com>
2025-11-30 01:52:54 +0700

    Ubah UI ke tema lebih sederhana dan tradisional

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+
 
 @app.route('/translate', methods=['POST'])
 def translate():
